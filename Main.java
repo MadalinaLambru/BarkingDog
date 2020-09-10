4b7d0fc4de121d0d448e6cb3b68d3818d451d440
MadalinaLambru <71075396+MadalinaLambru@users.noreply.github.com>
2020-09-11 00:36:29 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       boolean barkingDog = BarkingDog.shouldWakeUp(true,-1);
+        System.out.println(barkingDog);
+       // BarkingDog.shouldWakeUp(false,2);
+     //   BarkingDog.shouldWakeUp(true,8);
+      //  BarkingDog.shouldWakeUp(true,-1);
+    }
+}
